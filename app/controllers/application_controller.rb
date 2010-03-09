e786ab30761cf18f790589a8789f68de477e54e8
codesnik <aronaxis@gmail.com>
2010-03-09 05:13:23 +0300

    base authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,26 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  protected
+
+  # заглушка для эмуляции авторизованности
+  def current_user
+    @current_user ||= if session[:user_id]
+      User.find(session[:user_id])
+    end
+  end
+  helper_method :current_user
+
+  def login(user)
+    @current_user = user
+    session[:user_id] = user ? user.id : nil
+  end
+
+  # filters
+  def require_auth
+    unless current_user
+      redirect_to users_path, :alert => 'требуется аутентификация'
+    end
+  end
+
 end
