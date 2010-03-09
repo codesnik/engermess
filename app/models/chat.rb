a2ea7bb83a192b1871bbf83422938df191b5ad88
codesnik <aronaxis@gmail.com>
2010-03-09 05:32:59 +0300

    old fashioned tests for required attributes

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -1,4 +1,6 @@
 class Chat < ActiveRecord::Base
   has_many :user_chats
   has_many :users, :through => :user_chats
+
+  validates_presence_of :subject
 end
--- test/unit/chat_test.rb
+++ test/unit/chat_test.rb
@@ -1,8 +1,16 @@
 require 'test_helper'
 
 class ChatTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+
+  test "should create chat with valid attributes" do
+    assert_nothing_raised do
+      Chat.create! :subject => 'foobar'
+    end
+  end
+
+  test "should require subject" do
+    assert_raise ActiveRecord::RecordInvalid do
+      Chat.create! :subject => nil
+    end
   end
 end
