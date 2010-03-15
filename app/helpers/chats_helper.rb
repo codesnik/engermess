ac65927f91238e71cd8560e369780c8cc01354e5
codesnik <aronaxis@gmail.com>
2010-03-15 23:47:51 +0300

    display participants on chat index page

--- app/helpers/chats_helper.rb
+++ app/helpers/chats_helper.rb
@@ -2,4 +2,16 @@ module ChatsHelper
   def recipient_options_for_select
     options_from_collection_for_select(User.all - [current_user], :id, :name)
   end
+
+  def chat_users(chat)
+    chat.users.map {|user| user.name}.join(', ')
+  end
+
+  def chat_digest(chat)
+    if chat.messages.present?
+      truncate(chat.messages.last.body.squish, :length => 20)
+    else
+      '&mdash;'.html_safe
+    end
+  end
 end
--- test/unit/helpers/chats_helper_test.rb
+++ test/unit/helpers/chats_helper_test.rb
@@ -1,4 +1,18 @@
 require 'test_helper'
 
 class ChatsHelperTest < ActionView::TestCase
+  test "chat_users should show users in correct order" do
+    @chat = Chat.create! :subject => 'foobar'
+    @chat.users = users(:one), users(:two)
+
+    assert_equal chat_users(@chat), "#{users(:one).name}, #{users(:two).name}"
+  end
+
+  test "chat_users should show users in correct order, once again" do
+    # just in case fixtures are going in wrong order
+    @chat = Chat.create! :subject => 'foobar'
+    @chat.users = users(:two), users(:one)
+
+    assert_equal chat_users(@chat), "#{users(:two).name}, #{users(:one).name}"
+  end
 end
